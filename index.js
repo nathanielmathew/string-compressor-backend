2c6fe8499a7c85b8d4512399547e8226a874b0dc
Nathaniel <nathanielmat2@gmail.com>
2021-04-05 19:59:52 +0530

    add instructions

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ var app = express();
 var fs = require("fs");
 
 
-app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-      console.log( data );
-      res.end( data );
-   });
-})
+app.get('/', function (req, res) {
+    res.send("Enter a string request '/exampleString' to obtain compressed string");
+ })
 
 function compress(str){
     let compressed_str = [];
