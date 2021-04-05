07393e75d132866917fab597550cab7c9210f812
Nathaniel <nathanielmat2@gmail.com>
2021-04-05 23:39:16 +0530

    add decompression functionality

--- index.js
+++ index.js
@@ -6,8 +6,36 @@ var fs = require("fs");
 
 
 app.get('/', function (req, res) {
-    res.send("Enter a string request '/exampleString' to obtain compressed string");
+    res.send("Enter a string request 'compress/exampleString' to obtain compressed string or 'decompress/exampleString' to obtain decompressed string");
  })
+
+ function decompress(str){
+    let decompressed_str = [];
+    decompressed_str[0]=str.charAt(i);
+    let j = 1;
+    var counter=1;
+    for (var i = 1; i < str.length; i++) {
+        var previous = str.charAt(i-1);
+        var current = str.charAt(i);
+        if(current>='0' && current<='9'){
+            n = parseInt(current);
+            while(counter<n){
+                decompressed_str[j++]=previous;
+                counter++;
+            }
+            counter=1;
+        }
+        else {
+            decompressed_str[j++]=str.charAt(i);
+        }
+    }
+    decompressed_str = decompressed_str.join("");
+    myres = {
+           "str" : str,
+            "decompressed_str" : decompressed_str
+    }
+    return myres;
+}
 
 function compress(str){
     let compressed_str = [];
@@ -46,13 +74,18 @@ function compress(str){
     return myres;
 }
 
-app.get('/:str', function (req, res) {
+app.get('/compress/:str', function (req, res) {
        let myres=compress(req.params.str);
     //    console.log(myres.compressed_str);
        res.send(myres);
  })
 
- 
+ app.get('/decompress/:str', function (req, res) {
+    let myres=decompress(req.params.str);
+ //    console.log(myres.compressed_str);
+    res.send(myres);
+})
+
 const PORT = process.env.PORT || 3000;
 var server = app.listen(PORT, function () {
    var host = server.address().address
